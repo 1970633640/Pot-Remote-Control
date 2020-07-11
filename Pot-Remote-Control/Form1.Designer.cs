9bb3198a58d19b417295fd2afe67d0d89c3c1b53
1970633640 <13761968+1970633640@users.noreply.github.com>
2020-07-11 15:22:44 +0800

    1

---
+++ Pot-Remote-Control/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Pot_Remote_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(52, 14);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(90, 25);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Enabled";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(48, 145);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(190, 21);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "GitHub (Help, License...)";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 175);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.checkBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pot Remote Control";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Pot-Remote-Control/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Automation;
+using System.Windows.Forms;
+using SharpDX.XInput;
+
+namespace Pot_Remote_Control
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        string keycode = "";
+        Controller controller;
+        Gamepad gamepad;
+        State previousState;
+        public bool connected = false;
+        AutomationFocusChangedEventHandler focusHandler = null;
+        private bool timerCache=true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SubscribeToFocusChange();
+            controller = new Controller(UserIndex.One);
+            connected = controller.IsConnected;
+            if (!connected)
+            {
+                MessageBox.Show("Controller required!\nhttps://github.com", "No Xinput controller detected!");
+                this.Close();
+            }
+        }
+
+        private void OnFocusChange(object sender, AutomationFocusChangedEventArgs e)
+        {
+            AutomationElement focusedElement = sender as AutomationElement;
+            if (focusedElement != null)
+            {
+                int processId = focusedElement.Current.ProcessId;
+                using (Process process = Process.GetProcessById(processId))
+                {
+                    Debug.WriteLine(process.ProcessName);
+                    if (process.ProcessName == "PotPlayerMini64")
+                        timer1.Enabled = true;
+                    else
+                        timer1.Enabled = false;
+                }
+            }
+        }
+
+        private void SubscribeToFocusChange()
+        {
+            focusHandler = new AutomationFocusChangedEventHandler(OnFocusChange);
+            Automation.AddAutomationFocusChangedEventHandler(focusHandler);
+        }
+
+        private void UnsubscribeFocusChange()
+        {
+            if (focusHandler != null)
+            {
+                Automation.RemoveAutomationFocusChangedEventHandler(focusHandler);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                UnsubscribeFocusChange();
+                SubscribeToFocusChange();
+            }
+            else
+            {
+                timer1.Enabled = false;
+                UnsubscribeFocusChange();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var state = controller.GetState();
+
+            if (previousState.PacketNumber != state.PacketNumber)
+            {
+                gamepad = controller.GetState().Gamepad;
+                if (gamepad.Buttons == GamepadButtonFlags.A)
+                    SendKeys.SendWait(" ");
+                if (gamepad.Buttons == GamepadButtonFlags.B)
+                    SendKeys.SendWait("%{F4}");
+                if (gamepad.Buttons == GamepadButtonFlags.Start)
+                    SendKeys.SendWait("{ENTER}");
+                
+                if (gamepad.Buttons == GamepadButtonFlags.DPadRight && (!(gamepad.Buttons == GamepadButtonFlags.X)))
+                    SendKeys.SendWait("{RIGHT}");
+                if (gamepad.Buttons == GamepadButtonFlags.DPadLeft && (!(gamepad.Buttons == GamepadButtonFlags.X)))
+                    SendKeys.SendWait("{LEFT}");
+                if (gamepad.Buttons == GamepadButtonFlags.DPadUp && (!(gamepad.Buttons == GamepadButtonFlags.X)))
+                    keybd_event((byte)Keys.VolumeUp, 0, 0, 0); // increase volume
+                if (gamepad.Buttons == GamepadButtonFlags.DPadDown && (!(gamepad.Buttons == GamepadButtonFlags.X)))
+                    keybd_event((byte)Keys.VolumeDown, 0, 0, 0); // decrease volume
+
+                if (gamepad.Buttons == (GamepadButtonFlags.DPadRight | GamepadButtonFlags.X))
+                    SendKeys.SendWait("%{RIGHT}");
+                if (gamepad.Buttons == (GamepadButtonFlags.DPadLeft | GamepadButtonFlags.X))
+                    SendKeys.SendWait("%{LEFT}");
+                if (gamepad.Buttons == (GamepadButtonFlags.DPadUp |  GamepadButtonFlags.X))
+                    SendKeys.SendWait("%{UP}");
+                if (gamepad.Buttons == (GamepadButtonFlags.DPadDown | GamepadButtonFlags.X))
+                    SendKeys.SendWait("%{DOWN}");
+
+                if ((float)gamepad.RightTrigger < 5 && (float)gamepad.LeftTrigger < 5)
+                {
+                    timer2.Enabled = false;
+                    timerCache = true;
+                }
+
+                if ((float)gamepad.RightTrigger >= 5 && (float)gamepad.RightTrigger <= 100)
+                { timer2.Enabled = true; keycode = "{RIGHT}"; if (timerCache) timer2.Interval = 10; }
+                if ((float)gamepad.RightTrigger >= 101 && (float)gamepad.RightTrigger <= 150)
+                { timer2.Enabled = true; keycode = "^{RIGHT}"; if (timerCache) timer2.Interval = 10; }
+                if ((float)gamepad.RightTrigger >= 151 && (float)gamepad.RightTrigger <= 200)
+                { timer2.Enabled = true; keycode = "+{RIGHT}"; if (timerCache) timer2.Interval = 10; }
+                if ((float)gamepad.RightTrigger >= 201 && (float)gamepad.RightTrigger <= 255)
+                { timer2.Enabled = true; keycode = "^%{RIGHT}"; if (timerCache) timer2.Interval = 10; }
+
+                if ((float)gamepad.LeftTrigger >= 5 && (float)gamepad.LeftTrigger <= 100)
+                { timer2.Enabled = true; keycode = "{LEFT}"; if (timerCache) timer2.Interval = 10; }
+                if ((float)gamepad.LeftTrigger >= 101 && (float)gamepad.LeftTrigger <= 150)
+                { timer2.Enabled = true; keycode = "^{LEFT}"; if (timerCache) timer2.Interval = 10; }
+                if ((float)gamepad.LeftTrigger >= 151 && (float)gamepad.LeftTrigger <= 200)
+                { timer2.Enabled = true; keycode = "+{LEFT}"; if (timerCache) timer2.Interval = 10; }
+                if ((float)gamepad.LeftTrigger >= 201 && (float)gamepad.LeftTrigger <= 255)
+                { timer2.Enabled = true; keycode = "^%{LEFT}"; if (timerCache) timer2.Interval = 10; }
+            }
+            previousState = state;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            SendKeys.SendWait(keycode);
+            timerCache = false;
+            timer2.Interval = 500;
+        }
+    }
+}
